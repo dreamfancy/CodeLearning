123b9db3f03a7d0057de9cf5ea4a8139f6f57d5f
Chaoyang Fan <YC03CF1@federated.fds>
2017-04-03 16:36:41 -0700

    add mypower

--- CodeLearning_DFPC3/src/recursion/MyPower.java
+++ CodeLearning_DFPC3/src/recursion/MyPower.java
@@ -2,4 +2,17 @@ package recursion;
 
 public class MyPower {
 
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public long getMypow(int x, int n)
+	{
+		if(n==1) return (long)x;
+		
+		long val = getMypow(x,n/2);
+		if(n%2==0) return val*val;
+		else return val*val*x;
+	}
 }
