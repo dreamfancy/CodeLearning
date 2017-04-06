36c927867bf50e4d9adc77c54eeb16ff5ae72b43
Chaoyang Fan <chaoyangfan@gmail.com>
2017-04-05 22:44:14 -0700

    sync evening 040517

---
+++ CodeLearning_DFPC3/src/linkedList/LinkedListCycle_141.java
@@ -0,0 +1,44 @@
+package linkedList;
+
+import java.util.HashSet;
+
+public class LinkedListCycle_141 
+{
+	public static boolean hasCycle(ListNode head)
+	{
+		if(head == null) return false;
+		
+		ListNode first = head;
+		ListNode second = head;
+		
+		while(second.next !=null && second.next.next!=null)
+		{
+			first = first.next;
+			second = second.next.next;
+			if(first==second) return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
+	
+	//Solution_1 use HashSet to record repeat
+    public static boolean hasCycle_UseHashSet(ListNode head) 
+    {
+    	if(head==null) return false;
+    	
+    	HashSet<ListNode> hash = new HashSet<ListNode>();
+    	
+    	while(head.next!=null && !hash.contains(head))
+    	{
+    		hash.add(head);
+    		head=head.next;
+    	}
+    	
+    	if(hash.contains(head)) return true;
+    	
+    	return false;
+    }
+}
