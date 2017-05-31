228d11c3fad415854364c40e4b20e81c06604b93
Chaoyang Fan <chaoyangfan@gmail.com>
2017-05-30 22:38:20 -0700

    Stack and Queue

---
+++ CodeLearning_DFPC3/src/StackQueue/ImplementStackUsingQueue_225.java
@@ -0,0 +1,6 @@
+package StackQueue;
+
+public class ImplementStackUsingQueue_225 {
+	//Please see the class MyStack
+
+}
---
+++ CodeLearning_DFPC3/src/StackQueue/MyQueue.java
@@ -0,0 +1,101 @@
+package StackQueue;
+
+import java.util.Stack;
+
+public class MyQueue {
+
+	private Stack<Integer> s1,s2;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        
+    	s1 = new Stack<Integer>();
+    	s2 = new Stack<Integer>();
+    }
+    
+    public void push(int x) 
+    {
+    	s1.push(x);
+    
+    }
+
+    public int pop() 
+    {
+    	//Normal think
+    	/*
+    	if(!s2.isEmpty())
+    	{
+    		return s2.pop();
+    	}
+    	else
+    	{
+    		while(!s1.isEmpty())
+    		{
+    			s2.push(s1.pop());
+    		}
+    		return s2.isEmpty()? null:s2.pop();
+    	}
+    	*/
+    	
+    	//Optimizing:
+    	//Make the following if sentence as a method move(){}
+    	if(s2.isEmpty() && !s1.isEmpty())
+    	{
+    		s2.push(s1.pop());
+    	}
+    	
+    	return s2.isEmpty()? null:s2.pop();
+    }
+    
+    public int peek() 
+    {
+    	if(!s2.isEmpty())
+    	{
+    		return s2.peek();
+    	}
+    	else
+    	{
+    		while(!s1.isEmpty())
+    		{
+    			s2.push(s1.pop());
+    		}
+    		return s2.isEmpty()? null:s2.peek();
+    	}
+
+    }
+    public boolean empty() 
+    {
+    	return (s1.isEmpty()) && (s2.isEmpty());
+    	
+    }
+
+    
+    
+}
+    //Solution 1: My Solution, Keep Pop and Peek easy, make more work for push
+    /*    
+    public void push(int x) {
+        while(!s1.isEmpty())
+        {
+        	s2.push(s1.pop());        	
+        }
+        
+        s2.push(x);
+        while(!s2.isEmpty())
+        {
+        	s1.push(s2.pop());
+        }
+    }
+   
+    public int pop() {
+        return s1.pop();
+    }
+    
+    public int peek() {
+        return s1.peek();
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+*/
