42b6815b16ab368183e6cc84994319201a8c4d52
Chaoyang Fan <chaoyangfan@gmail.com>
2017-07-07 21:55:46 -0700

    subset 78

--- CodeLearning_DFPC3/src/stackqueueheap/Subsets_78.java
+++ CodeLearning_DFPC3/src/stackqueueheap/Subsets_78.java
@@ -1,5 +1,41 @@
 package stackqueueheap;
 
-public enum Subsets_78 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Subsets_78 
+{
+    public List<List<Integer>> subsets(int[] nums) 
+    {
+    	//List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	List<List<Integer>> curSol = new ArrayList<List<Integer>>();
+
+    	curSol.add(new ArrayList<Integer>());
+    	//return dfs(nums,0,curSol,result);
+    	return dfs(nums,0,curSol);
+    }
+
+    public List<List<Integer>> dfs(int[] arr, int index,List<List<Integer>> curSol)
+    {
+    	int curElement = arr[index];
+    	int curSize = curSol.size();
+    	
+    	for(int i=0; i<curSize; i++)
+    	{
+    		List<Integer> curList = curSol.get(i);
+            List<Integer> newList = new ArrayList<Integer>();
+    		newList.addAll(curList);
+            newList.add(curElement);
+    		curSol.add(newList);
+    	}
+    	index++;
+    	
+    	if(index<arr.length)
+    	{
+    		return  dfs(arr,index,curSol);
+    	}
+    	
+    	return curSol;
+    	
+    }
 }
