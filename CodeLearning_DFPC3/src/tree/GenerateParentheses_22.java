dd6923ef1fa7691e976f20ba8c14673d7b7df5bc
Chaoyang Fan <chaoyang.fan@macys.com>
2017-07-11 16:21:19 -0700

    071117 office

--- CodeLearning_DFPC3/src/tree/GenerateParentheses_22.java
+++ CodeLearning_DFPC3/src/tree/GenerateParentheses_22.java
@@ -17,19 +17,24 @@ public class GenerateParentheses_22 {
     	if(curL + curR == n*2)
     	{
     		res.add(sb.toString());
+    		return res;
     	}
-    	else if(curL==curR)
+    	if(curL<n)
     	{
     		sb.append("(");
     		res = helper(n,curL+1,curR,sb,res);
+    		sb.deleteCharAt(sb.length()-1);  
+    		//sb.setLength(sb.length()-1);
     	}
-    	else if(curL>curR)
+    	if(curL>curR)
     	{
-    		sb.append("(");
-    		res = helper(n,curL+1,curR,sb,res);
-    		sb.deleteCharAt(sb.length()-1);
+    		//sb.append("(");
+    		//res = helper(n,curL+1,curR,sb,res);
     		sb.append(")");
     		res = helper(n,curL,curR+1,sb,res);
+    		sb.deleteCharAt(sb.length()-1);  //It is necessary!!!
+    		//Attention because of using recursion, we should always make it clean even in the last step in the method
+    		//The sb is used across all the recursion levels. 
     	}
     	return res;
     }
