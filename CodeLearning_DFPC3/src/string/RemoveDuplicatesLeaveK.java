c5d6d626bc14f251778af9cb4644d36165cb29eb
Chaoyang Fan <chaoyang.fan@macys.com>
2017-08-01 23:57:39 -0700

    080117

--- CodeLearning_DFPC3/src/string/RemoveDuplicatesLeaveK.java
+++ CodeLearning_DFPC3/src/string/RemoveDuplicatesLeaveK.java
@@ -1,7 +1,9 @@
 package string;
 
 public class RemoveDuplicatesLeaveK{
-
+    
+	//Solution 1: Lecture Solution
+	//assign slow then++,result is [0,slow) 
 	public String removeDuplicatesLeaveK(String s, int k)
 	{
 		if(s==null || s.length()<=k) return s;
@@ -9,14 +11,31 @@ public class RemoveDuplicatesLeaveK{
 		char[] chars = s.toCharArray();
 		
 		int slow = k;
-		for(int fast=k; fast<s.length();fast++)
+		for(int fast=k; fast<s.length();fast ++)
 		{
-			if(chars[fast]!=chars[fast-k])
+			if(chars[fast]!=chars[slow -k])
 			{
 				chars[slow++] = chars[fast];
 			}
 		}
 		
-		return new String(chars,0,slow-1);
+		return new String(chars,0,slow);
 	}
+	
+		//Solution 2: Lecture Solution
+		//slow++ then assign,result is [0,slow] 
+		public String removeDuplicatesLeaveK_2(String s, int k)
+		{
+			if(s==null || s.length()<=k) return s;
+			char[] chars = s.toCharArray();
+			int slow = k-1;
+			for(int fast=k-1; fast<chars.length; fast++)
+			{
+				if(chars[fast]!= chars[slow-k+1])
+				chars[++slow] = chars[fast];
+			}
+			
+			return new String(chars,0,slow+1);
+			
+		}
 }
