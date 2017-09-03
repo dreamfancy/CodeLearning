0a14683a6ce39087b5c73ff695d5f162ae034633
Chaoyang Fan <chaoyang.fan@macys.com>
2017-09-02 23:55:38 -0700

    zigzag code

--- CodeLearning_DFPC3/src/first100/PalindromeNumber_9.java
+++ CodeLearning_DFPC3/src/first100/PalindromeNumber_9.java
@@ -2,7 +2,21 @@ package first100;
 
 public class PalindromeNumber_9 
 {
-    public boolean isPalindrome(int x) {
+	//Solution 2: bit operation
+    public boolean isPalindrome_2(int x) {
+        if(x<0||(x>0&&x%10==0)) return false;
+        int reverse=0;
+        while(x>reverse){
+            reverse=reverse*10+x%10;//Revisit!
+            x/=10;
+        }
+        return x==reverse||x==reverse/10;
+    }
+	
+	
+	
+	//My Solution : Integer to String
+    public boolean isPalindrome_1(int x) {
     	
         String s = Integer.toString(x);
         int len = s.length();
@@ -14,6 +28,8 @@ public class PalindromeNumber_9
         {
             if(s.charAt(left)==s.charAt(right))
             {
+            	left++;
+            	right--;
                 continue;
             }
             return false;
