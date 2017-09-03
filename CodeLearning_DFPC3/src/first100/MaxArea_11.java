911e92d3cbd2b998d18b7ea58508b07f420062b8
Chaoyang Fan <chaoyang.fan@macys.com>
2017-09-03 13:01:46 -0700

    max area two pointer move from far to near

--- CodeLearning_DFPC3/src/first100/MaxArea_11.java
+++ CodeLearning_DFPC3/src/first100/MaxArea_11.java
@@ -2,8 +2,27 @@ package first100;
 
 public class MaxArea_11 {
 	
-	//Solution Exceed Max Limit
-    public int maxArea(int[] height) 
+	//Solution 2: Two Pointer: Use the opposite direction to decide the
+	//best solution, When to move left && When to move right!!!
+    public int maxArea_2(int[] height) 
+    {
+    	if(height == null || height.length<2) return -1;
+        int size = height.length;
+        int max = Math.min(height[0], height[size-1]) * (size-1);
+        int left=0, right=size-1;
+        
+        while(left<right)
+        {
+           max = Math.max(max, Math.min(height[left],height[right])*(right-left));
+           if(height[left]<=height[right]) left++;
+           else right--;
+        }
+        return max; 
+    }
+	
+	
+	//Solution 1 Exceed Max Time Limit
+    public int maxArea_1(int[] height) 
     {
     	if(height == null || height.length<2) return -1;
         int max = Math.min(height[0], height[1]);
