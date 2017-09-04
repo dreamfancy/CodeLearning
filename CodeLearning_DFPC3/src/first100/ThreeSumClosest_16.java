77f0d20a585957e5f36a1631bd4dcf5bdbf2e248
Chaoyang Fan <chaoyang.fan@macys.com>
2017-09-04 13:31:41 -0700

    threesumclosest

--- CodeLearning_DFPC3/src/first100/ThreeSumClosest_16.java
+++ CodeLearning_DFPC3/src/first100/ThreeSumClosest_16.java
@@ -4,15 +4,20 @@ import java.util.Arrays;
 
 public class ThreeSumClosest_16 
 {    
-	public int threeSumClosest(int[] nums, int target) 
+	
+	//Solution 1: My Solution: 3 pointers based on two pointers in reverse
+	//direction + another for loop
+	public int threeSumClosest_MySolution(int[] nums, int target) 
 	{
 		if(nums==null || nums.length==0) return 0;
 		if(nums.length==1) return nums[0];
 		if(nums.length==2) return nums[0]+nums[1];
-		
+		//or  throw new IllegalArgumentException();
+
 		Arrays.sort(nums);
 		
-		int min = Math.abs(nums[0]+nums[1]+nums[2]-target);
+        int val = nums[0]+nums[1]+nums[2];
+		int min = Math.abs(val-target);
 		
 		for(int i=0; i<nums.length-2; i++)
 		{
@@ -21,18 +26,19 @@ public class ThreeSumClosest_16
 			
 			while(j<k)
 			{
-				int cur = nums[0]+nums[1]+nums[2]-target;
+				int cur = nums[i]+nums[j]+nums[k]-target;
 				if(Math.abs(cur)<min)
 				{
+                    val = nums[i]+nums[j]+nums[k];
 					min =Math.abs(cur);
-					if(min==0) return 0;
-					else if(min<0) j++;
-					else k--;
+					if(min==0) return val;
+					//else if(cur<0) j++;
+					//else k--;
 				}
 				if (cur>0) k--;
 				else j++;
 			}
 		}
-		return min;
+		return val;
 	}
 }
