648ffa68e5e44df61bd269a01aeb0a9ef84d8624
Chaoyang Fan <chaoyang.fan@macys.com>
2017-09-04 21:05:49 -0700

    removeduplicatesfromsortedarray

--- CodeLearning_DFPC3/src/first100/RemoveDuplicatesFromSortedArray_26.java
+++ CodeLearning_DFPC3/src/first100/RemoveDuplicatesFromSortedArray_26.java
@@ -2,6 +2,7 @@ package first100;
 
 public class RemoveDuplicatesFromSortedArray_26 
 {
+	//My Solution:
     public int removeDuplicates(int[] nums) 
     {
     	if(nums==null || nums.length==0) return 0;
@@ -16,6 +17,18 @@ public class RemoveDuplicatesFromSortedArray_26
     		fast++;
     	}
     	return slow;
+    }
+    
+    //Similar To My Solution but treat i=0 as one condition of if
+    public int removeDuplicates_2(int[] nums) {
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (right == 0 || nums[right] != nums[right-1]) {
+                nums[left++] = nums[right];
+            }
+            right++;
+        }
+        return left;
     }
 
 
