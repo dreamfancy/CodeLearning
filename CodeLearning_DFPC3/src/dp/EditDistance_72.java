77b91d592a33012384befcd8e908a7d26a17cacc
Chaoyang Fan <chaoyang.fan@macys.com>
2017-09-15 22:40:34 -0700

    dp edit path

---
+++ CodeLearning_DFPC3/src/dp/EditDistance_72.java
@@ -0,0 +1,48 @@
+ package dp;
+
+public class EditDistance_72 {
+	
+	//dp[i][j] = dp[i-1][j-1] if(s1[i-1] = s2[j-1]
+	//           min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])+1 if(s1[i-1]!=s2[j-1] 
+	
+    public int minDistance(String word1, String word2) 
+    {
+    	if(word1==null && word2==null) return 0;
+    	else if(word1==null) return word2.length();
+    	else if(word2==null) return word1.length();
+    	else if(word1.length()==0) return word2.length();
+    	else if(word2.length()==0) return word1.length();
+    	
+    	int len1 = word1.length();
+    	int len2 = word2.length();
+    	
+    	int[][] dp = new int[len1+1][len2+1]; 
+        
+    	dp[0][0] = 0;
+        for(int i=1; i<=len1; i++)
+        {
+        	dp[i][0] = i;
+        }
+        for(int j=1; j<=len2; j++)
+        {
+        	dp[0][j] = j;
+        }
+        
+        for(int i=1; i<=len1; i++)
+        {
+        	for(int j=1; j<=len2; j++)
+        	{
+        		if(word1.charAt(i-1)==word2.charAt(j-1))
+        		{
+        			dp[i][j] = dp[i-1][j-1];
+        		}
+        		else
+        		{
+        			dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]))+1;
+        		}
+        	}
+        }
+    	
+    	return dp[len1][len2];
+    }
+}
