33912f7aec18bb6a6f0e82b43e0af777ce5c85d1
Chaoyang Fan <chaoyang.fan@macys.com>
2017-09-19 13:37:43 -0700

    rotateImage

--- CodeLearning_DFPC3/src/recursion/RotateImage_48.java
+++ CodeLearning_DFPC3/src/recursion/RotateImage_48.java
@@ -12,6 +12,7 @@ public class RotateImage_48
 	}
 	
 	//public void rotate(int[][] matrix, int start, int h_length, int v_length)
+	//Control for loop as 0->n-2 without change!!!
 	public void rotate(int[][] matrix, int start, int n)
 	{
 		if(n==0 || n==1) return;
@@ -22,20 +23,19 @@ public class RotateImage_48
 		}
 		for(int i=0; i<n-1; i++)
 		{
-			matrix[start][start+i] = matrix[start+n-2][start+i];
+			matrix[start][start+i] = matrix[start+n-1-i][start];
 		}
 		for(int i=0; i<n-1; i++)
 		{
-			matrix[start+i][start+n-2] = matrix[start+n-2][start+n-2-i];
+			matrix[start+n-1-i][start] = matrix[start+n-1][start+n-1-i];
 		}
 		for(int i=0; i<n-1; i++)
 		{
-			matrix[start+n-2][start+n-2-i] = matrix[start+n-2-i][start];
+			matrix[start+n-1][start+n-1-i] = matrix[start+i][start+n-1];
 		}
-		
 		for(int i=0; i<n-1; i++)
 		{
-			matrix[start+n-2-i][start] = temp[i];
+			matrix[start+i][start+n-1] = temp[i];
 		}
 		rotate(matrix,start+1,n-2);
 	}
