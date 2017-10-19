24a209eb7eec1ee3971e371648175c03ab2689a4
Chaoyang Fan <chaoyang.fan@macys.com>
2017-10-19 11:27:11 -0700

    whileloop for insert interval

--- CodeLearning_DFPC3/src/leetcode51to100/InsertInterval_57.java
+++ CodeLearning_DFPC3/src/leetcode51to100/InsertInterval_57.java
@@ -3,10 +3,34 @@ package leetcode51to100;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InsertInterval_57 
 {
+	//Solution 2:
+	//As Cha Ru can have different scenarios, while loop is better than for loop in this case
+	public List<Interval> insert_2(List<Interval> intervals, Interval newInterval) {
+	    List<Interval> result = new LinkedList<>();
+	    int i = 0;
+	    // add all the intervals ending before newInterval starts
+	    while (i < intervals.size() && intervals.get(i).end < newInterval.start)
+	        result.add(intervals.get(i++));
+	    // merge all overlapping intervals to one considering newInterval
+	    while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+	        newInterval = new Interval( // we could mutate newInterval here also
+	                Math.min(newInterval.start, intervals.get(i).start),
+	                Math.max(newInterval.end, intervals.get(i).end));
+	        i++;
+	    }
+	    result.add(newInterval); // add the union of intervals we got
+	    // add all the rest
+	    while (i < intervals.size()) result.add(intervals.get(i++)); 
+	    return result;
+	}
+	
+	
+	
 	//My Solution:
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) 
     {
